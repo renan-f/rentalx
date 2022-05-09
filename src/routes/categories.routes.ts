e9ebf338fcb37599672ab355e8eefd59382d7ad9
Renan Filgueiras Andrade <filgueiras.rfa@gmail.com>
2022-05-09 20:10:11 -0300

    Aplicação de conceitos SOLID

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,21 +1,18 @@
 import { Router } from 'express';
-import { Category } from '../model/Category';
 import { CategoriesRepository } from '../repositories/CategoriesRepository';
+import { MysqlCategoriesRepository } from '../repositories/MysqlCategoriesRepository';
+import { CreateCategoryService } from '../services/CreateCategoryServices';
 
 const categoriesRoutes = Router();
 
+// const categoriesRepository = new MysqlCategoriesRepository();
 const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post('/', (request, response) => {
     const { name, description } = request.body;
 
-    const categoryAlreadyExists = categoriesRepository.findByName(name);
-
-    if(categoryAlreadyExists) {
-        return response.status(400).json({ error : 'Category already exists' });
-    }
-
-    categoriesRepository.create({ name, description });
+    const createCategoryService = new CreateCategoryService(categoriesRepository);
+    createCategoryService.execute({ name, description });
 
     return response.status(201).send();
 
