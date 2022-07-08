9cfdd3b5d825e67078f8b046ec4e7b0708df7ef9
Renan Filgueiras Andrade <filgueiras.rfa@gmail.com>
2022-07-07 21:58:05 -0300

    Aplicando injessão de dependência com TSyringe

--- src/modules/cars/useCases/createCategory/CreateCategoryController.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.ts
@@ -1,15 +1,16 @@
-import { Request, Response } from "express";
-import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
+import { Request, Response } from 'express';
+import { CreateCategoryUseCase } from './CreateCategoryUseCase';
 
+import { container } from 'tsyringe';
 
 class CreateCategoryController {
-    constructor(private createCategoryUseCase: CreateCategoryUseCase) {}
 
-    async handle(request: Request, response: Response):Promise<Response> {
+    async handle(request: Request, response: Response): Promise<Response> {
         const { name, description } = request.body;
+        const createCategoryUseCase = container.resolve(CreateCategoryUseCase);
 
         try {
-            await this.createCategoryUseCase.execute({ name, description });
+            await createCategoryUseCase.execute({ name, description });
         } catch (error) {
             return response.status(400).json({ message: (error as Error).message });
         }
