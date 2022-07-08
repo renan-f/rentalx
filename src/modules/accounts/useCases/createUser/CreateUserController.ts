c4d9cc9cfe9acb5494f55a5c238212020aee7895
Renan Filgueiras Andrade <filgueiras.rfa@gmail.com>
2022-07-08 13:19:16 -0300

    Criação de usuário: Repository, UseCase e Rota

---
+++ src/modules/accounts/useCases/createUser/CreateUserController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+import { CreateUserUseCase } from "./CreateUserUseCase";
+
+class CreateUserController {
+    async handle(request: Request, respose: Response): Promise<Response> {
+        const { name, username, email, password, driver_license } = request.body;
+        const createUserUseCase = container.resolve(CreateUserUseCase);
+
+        await createUserUseCase.execute({ name, username, email, password, driver_license });
+
+        return respose.status(201).send();
+    }
+}
+
+export { CreateUserController };
