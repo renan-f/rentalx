f18b09d74455b1646a4d5baa30216b05371c335b
Renan Filgueiras Andrade <filgueiras.rfa@gmail.com>
2022-07-31 16:15:30 -0300

    Refatoração dos imports

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -1,6 +1,6 @@
 import { inject, injectable } from "tsyringe";
 
-import { deleteFile } from "../../../../utils/file";
+import { deleteFile } from "@utils/file";
 
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -3,8 +3,8 @@ import multer from "multer";
 
 import uploadConfig from "../config/upload";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
-import { CreateUserController } from "../modules/accounts/useCases/createUser/CreateUserController";
-import { UpdateUserAvatarController } from "../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
+import { CreateUserController } from "@modules/accounts/useCases/createUser/CreateUserController";
+import { UpdateUserAvatarController } from "@modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
 
 const usersRoutes = Router();
 
