bc7b9be7816bfae2dce3bdbc5ee8822ddf9c200b
Renan Filgueiras Andrade <filgueiras.rfa@gmail.com>
2022-07-31 17:10:46 -0300

    Refatoração de estrutura dos arquivos separando infra

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -2,7 +2,7 @@ import fs from 'fs';
 import { parse as csvParse } from 'csv-parse';
 import { inject, injectable } from 'tsyringe';
 
-import { CategoriesRepository } from '@modules/cars/repositories/implementations/CategoriesRepository';
+import { CategoriesRepository } from '@modules/cars/infra/typeorm/repositories/CategoriesRepository';
 
 interface IImportCategory {
     name: string;
